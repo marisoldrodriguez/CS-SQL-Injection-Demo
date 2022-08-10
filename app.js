870f4bab08a91768bfa7b7fd52fd54e377b0dcfb
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-08-10 12:17:08 -0400

    Added a second user to database

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
  db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
  db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+ db.run("INSERT INTO user VALUES ('privilegedUserExtra', 'privilegedUser2', 'Administrator2')");
 });
 
 app.get('/', function (req, res) {
